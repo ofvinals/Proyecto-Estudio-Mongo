8ae24fddd0b9fb512242480858fef194652de657
Oscar Frias Viñals <140022816+ofvinals@users.noreply.github.com>
2024-05-05 11:27:01 -0300

    corrijo vercel

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -30,6 +30,7 @@ app.use(
 			'http://localhost:5173',
 			'https://proyecto-estudio-mongo.vercel.app',
 			'https://proyecto-estudio-mongo.onrender.com',
+			
 		],
 		credentials: true,
 		optionsSuccessStatus: 200,
